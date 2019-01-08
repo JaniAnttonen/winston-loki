a51af1696ae92b3dfaf3b6c24e1bf6985a6fe27a
Jani Anttonen <jantto@utu.fi>
2019-01-09 00:02:22 +0200

    add tests for transport, batcher

--- test/batcher.test.js
+++ test/batcher.test.js
@@ -1,8 +1,26 @@
 /* eslint-env jest */
-// const Batcher = require('../src/batcher')
+const Batcher = require('../src/batcher')
+const fixtures = require('./fixtures.json')
 
-describe('ebin', function () {
-  it('should be true', function () {
-    expect(true).toBe(true)
+describe('Batcher tests', function () {
+  it('Should add same items as separate streams', function () {
+    const batcher = new Batcher(fixtures.options)
+    batcher.pushLogEntry(fixtures.logs_mapped[0])
+    batcher.pushLogEntry(fixtures.logs_mapped[0])
+    expect(batcher.batch.streams.length).toBe(2)
+  })
+  it('Should add items with same labels as separate streams', function () {
+    const batcher = new Batcher(fixtures.options)
+    batcher.pushLogEntry(fixtures.logs_mapped[0])
+    batcher.pushLogEntry(fixtures.logs_mapped[2])
+    expect(batcher.batch.streams.length).toBe(2)
+  })
+  it('Should be able to clear the batch of streams', function () {
+    const batcher = new Batcher(fixtures.options)
+    batcher.pushLogEntry(fixtures.logs_mapped[0])
+    batcher.pushLogEntry(fixtures.logs_mapped[2])
+    expect(batcher.batch.streams.length).toBe(2)
+    batcher.clearBatch()
+    expect(batcher.batch.streams.length).toBe(0)
   })
 })
