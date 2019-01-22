ddc938bbd87cc13c3ce904917ddfe933a11f9a2c
Jani Anttonen <jantto@utu.fi>
2019-01-22 11:31:51 +0200

    jani u stupid

--- src/proto/helpers.js
+++ src/proto/helpers.js
@@ -1,17 +1,16 @@
 const moment = require('moment')
 module.exports = {
-  createProtoTimestamps: logEntry =>
-    logEntry.map(logEntry => {
-      logEntry.entries.map(entry => {
-        const dt = moment(entry.ts).valueOf()
-        return {
-          timestamp: {
-            seconds: Math.floor(dt / 1000),
-            nanos: (dt % 1000) * 1000
-          },
-          line: entry.line
-        }
-      })
-      return logEntry
+  createProtoTimestamps: logEntry => {
+    logEntry.entries = logEntry.entries.map(entry => {
+      const dt = moment(entry.ts).valueOf()
+      return {
+        timestamp: {
+          seconds: Math.floor(dt / 1000),
+          nanos: (dt % 1000) * 1000
+        },
+        line: entry.line
+      }
     })
+    return logEntry
+  }
 }
